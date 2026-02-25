0482bb377912e8c4b89039e8ccb1b99ab7e711e1
AminiNegar <89100880+AminiNegar@users.noreply.github.com>
2026-02-25 06:05:11 +0330

    Update index.js

--- uber_projet/server/index.js
+++ uber_projet/server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const pool = new Pool({
     user : 'postgres' , 
     host : 'localhost' , 
-    database : 'UberDB' , // نام دیتابیس طبق تصویر شما
+    database : 'UberDB' , 
     password : '4043614002' , 
     port : 5432
 })
@@ -38,20 +38,16 @@ app.get("/trips", async (req, res) => {
   }
 });
 
-// بخشی از فایل server/index.js
 app.post("/trips", async (req, res) => {
   try {
     const { date, time, vehicle_type, payment_method, customer_rating } = req.body;
 
-    // 1. محاسبه دستی روز و ساعت
     const d = new Date(date);
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const my_day = days[d.getDay()]; // محاسبه نام روز
-    const my_hour = parseInt(time.split(":")[0]); // محاسبه ساعت
-
+    const my_day = days[d.getDay()]; 
+    const my_hour = parseInt(time.split(":")[0]); 
     const booking_id = "NEW" + Math.floor(Math.random() * 100000);
 
-    // 2. ارسال به دیتابیس (دقت کنید که $7 و $8 اضافه شده‌اند)
     const newTrip = await pool.query(
       `INSERT INTO gold.dataset (
          booking_id, date, time, vehicle_type, payment_method, customer_rating, booking_status, 
@@ -67,7 +63,6 @@ app.post("/trips", async (req, res) => {
     res.status(500).send(err.message);
   }
 });
-// edit trip or state of the trip
 app.put("/trips/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -84,7 +79,6 @@ app.put("/trips/:id", async (req, res) => {
   }
 });
 
-// delete trip
 app.delete("/trips/:id", async (req, res) => {
   try {
     const { id } = req.params;
